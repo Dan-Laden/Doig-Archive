d900073625ed39241dff5aaa35bc44578455bdad
Dan-Laden <dthomasladen@gmail.com>
2018-04-06 16:34:22 -0600

    pdf reading

--- code/test.py
+++ code/test.py
@@ -1,1 +1,14 @@
+import nltk, PyPDF2
+
 print("Test File")
+
+f = open("source/This-House-of-Sky-1-5.pdf", 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(f)
+
+page = pdfReader.getPage(0)
+
+for i in range(0, pdfReader.getNumPages()):
+    print("==========================\n=\tPage "+(str)(i+1)+"\n==========================")
+    page = pdfReader.getPage(i)
+    print(page.extractText())
