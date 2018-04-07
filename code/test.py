ec1d05f9ce4fb42320b9d90a487902fefaf8516c
Dan-Laden <dthomasladen@gmail.com>
2018-04-07 17:02:53 -0600

    NLTK testing

--- code/test.py
+++ code/test.py
@@ -1,14 +1,55 @@
+#
+#author@ Daniel Laden
+#email@ dthomasladen@gmail.com
+#
+#resources used for code so far
+#
+# https://stackoverflow.com/questions/15547409/how-to-get-rid-of-punctuation-using-nltk-tokenizer
+# https://github.com/nltk/nltk/wiki/Frequently-Asked-Questions-(Stackoverflow-Edition)
+
+
+
+
+
 import nltk, PyPDF2
+import string
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
 
 print("Test File")
 
 f = open("source/This-House-of-Sky-1-5.pdf", 'rb')
 
 pdfReader = PyPDF2.PdfFileReader(f)
+text = ""
 
 page = pdfReader.getPage(0)
 
 for i in range(0, pdfReader.getNumPages()):
     print("==========================\n=\tPage "+(str)(i+1)+"\n==========================")
     page = pdfReader.getPage(i)
+    text = text + page.extractText()
     print(page.extractText())
+
+translate_table = dict((ord(char), None) for char in string.punctuation)
+text = text.translate(translate_table)
+print(text)
+print("==========================\n=\tWithout stop removal "+(str)(len(text.split()))+"\n==========================")
+
+stoplist = set(stopwords.words('english'))
+
+tokenized_set = word_tokenize(text.lower())
+tokenized_sent_nostop = [token for token in tokenized_set if token not in stoplist]
+
+print(tokenized_sent_nostop)
+print("==========================\n=\tWith stop removal and .lower "+(str)(len(tokenized_sent_nostop))+"\n==========================")
+
+tokenized_set = word_tokenize(text)
+tokenized_sent_nostop = [token for token in tokenized_set if token not in stoplist]
+
+print(tokenized_sent_nostop)
+print("==========================\n=\tWith stop removal and no .lower "+(str)(len(tokenized_sent_nostop))+"\n==========================")
