0a1fc418b16c5d96140b72a6eca9804dd3bd03ba
Dan-Laden <dthomasladen@gmail.com>
2018-05-09 11:24:49 -0600

    added weight calculations for sql intergration later

--- code/test-relations.py
+++ code/test-relations.py
@@ -16,6 +16,11 @@ class Relation:
         self.keyword = inKeyword
         self.occurrences = inOccurrences
 
+class Weight:
+    def __init__(self, inSource, inWeight):
+        self.source = inSource
+        self.weight = inWeight
+
 def buildRelations(f):
     filename = os.path.basename(f.name)
 
@@ -23,18 +28,46 @@ def buildRelations(f):
     splitKeys = keywords.split("; ")
     freq_set = FreqDist(splitKeys)
 
-    relation_arr = []
+    relation_dic = {}
 
     for word in freq_set:
         if freq_set[word] > 9:
-            relation_arr.append(Relation(filename, word, freq_set[word]))
+            relation_dic[word] = Relation(filename, word, freq_set[word])
 
-    return relation_arr
+    return relation_dic
+
+def compareRelations(d1, d2):
+    relation_sql = {}
+    for key1 in d1:
+        print(key1+"\n-----------")
+        for key2 in d2:
+            print(key2+"\n_________")
+            if key1 == key2:
+                tag = d1[key1].source + "-" + key1
+                relation_sql[tag] = Weight(d2[key2].source, computeWeight(d1[key1].occurrences, d2[key2].occurrences))
+
+    return relation_sql
 
 
-file = open("testkey.txt", "r")
+def computeWeight(v1, v2):
+    holder = v1 - v2
+    if holder < 0:
+        holder = -holder
+    holder = (v1 - holder)/2
+    return holder
 
-rel_arr = buildRelations(file)
+
+file1 = open("testkey.txt", "r")
+file2 = open("testwrite.txt", "r")
+
+rel_arr1 = buildRelations(file1)
+file1 = open("testkey.txt", "r")
+rel_arr3 = buildRelations(file1)
+
+weight_db = compareRelations(rel_arr1, rel_arr3)
+
+for key in weight_db:
+    print(key+": source "+weight_db[key].source+", weight "+ (str)(weight_db[key].weight)+"\n___________")
 
 #print(rel_arr[4].source + ", " + rel_arr[4].keyword + ", " + (str)(rel_arr[4].occurrences))
 
