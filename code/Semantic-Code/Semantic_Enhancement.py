bcebb6c113f1eff5817cebfb080e7a2d1509236b
Dan-Laden <dthomasladen@gmail.com>
2018-06-19 10:11:50 -0600

    added the start of the code for the sentiment analysis

--- code/Semantic-Code/Semantic_Enhancement.py
+++ code/Semantic-Code/Semantic_Enhancement.py
@@ -207,6 +207,7 @@ def clearItemDB():
         `RelatedBook`	TEXT,
         `Geolocation`	TEXT,
         `Img`	TEXT,
+        'Emotion'   TEXT,
         PRIMARY KEY(ID)
         );"""
         cursor.execute(sql_createtb)
@@ -371,6 +372,38 @@ def makeRelations(relationList, nameOfFiles):
 
     return connectedList
 
+#This function returns the sentiment values for the entered in text
+def getSentiment(example_sentence):
+    f=open("NRC-Emotion-Lexicon-Wordlevel-v0.92.txt", 'r')
+    active_word = " "
+    emotion_dictionary = {}
+    for line in f:
+        words = line.split()
+        empty_dict = {}
+        if words[0] != active_word:
+            active_word = words[0]
+            emotion_dictionary[words[0]] = empty_dict
+
+        emotion_dictionary[words[0]][words[1]] = (int)(words[2])
+
+    example_sentence = example_sentence.split()
+
+    f.close()
+
+    positivity_rating = 0
+    emotions = {"anger" : 0, "disgust" : 0, "fear" : 0, "joy" : 0, "sadness" : 0}
+    for word in example_sentence:
+        if word in emotion_dictionary:
+            #print(word + " : " + (str)(emotion_dictionary[word]))
+            for emotion in emotions:
+                emotions[emotion] = emotions[emotion] + emotion_dictionary[word][emotion]
+
+            positivity_rating = positivity_rating + emotion_dictionary[word]["positive"] - emotion_dictionary[word]["negative"]
+
+
+    #TODO calculate if the emotion is best suited for how positive the text is
+    print(emotions)
+    print("positive rating : "+(str)(positivity_rating))
 
 #End of functions for data parsing
 #########################
